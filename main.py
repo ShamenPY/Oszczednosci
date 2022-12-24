4164db88537c5d21b8369f4b681f97f59164dfb2
ShamenPY <karolswwacha17@gmail.com>
2022-12-24 21:35:20 +0100

    has been added common texts, entries and buttons in tkinter, First commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+class App(tk.Frame):
+    def __init__(self):
+
+
+
+        root = tk.Tk()
+        frm = ttk.Frame(root,padding=10)
+        root.geometry("960x1024")
+        root.title("Oszczędności")
+
+        message = tk.Label(root, text="Nazwa Transakcji").grid(column=1,row=5)
+        message = tk.Label(root, text="Kwota").grid(column=2, row=5)
+        message = tk.Label(root, text="Data ").grid(column=3, row=5)
+        message = tk.Label(root, text="    Opis").grid(column=4, row=5)
+
+        message = tk.Label(root, text="Nazwa Transakcji").grid(column=1,row=8)
+        message = tk.Label(root, text="Kwota").grid(column=2, row=8)
+        message = tk.Label(root, text="Data ").grid(column=3, row=8)
+        message = tk.Label(root, text="    Opis").grid(column=4, row=8)
+        nazwa1 = tk.StringVar()
+        kwota1 = tk.StringVar()
+        data1 = tk.StringVar()
+        opis1 = tk.StringVar()
+
+        def submitValues():
+            print(nazwa1.get(),kwota1.get(),data1.get(),opis1.get())
+
+        nazwatransakcji = tk.Entry(root,textvariable=nazwa1 ).grid(column=1,row=6)
+        kwotatransakcji = tk.Entry(root,textvariable=kwota1 ).grid(column=2,row=6)
+        datatransakcji = tk.Entry(root,textvariable=data1).grid(column=3,row=6)
+        opistransakcji = tk.Entry(root,textvariable=opis1 ).grid(column=4,row=6)
+
+
+        submit = tk.Button(root, text="Zapisz", command=submitValues).grid(column=6,row=5)
+        frm.grid()
+
+        dodaj_transakcje = ttk.Button(root, text="Dodaj Transakcję", command=Transakcja.dodaj).grid(column=1,row=1)
+        usun_transakcje = ttk.Button(root, text="Usuń Transakcję", command=Transakcja.usun).grid(column=2, row=1)
+
+
+
+
+        root.mainloop()
+class Transakcja():
+    def __init__(self):
+        pass
+    def kwota(self):
+        pass
+    def data(selfO):
+        pass
+    def opis(self):
+        pass
+
+    def dodaj():
+        print("Dodawanie Transakcji Test")
+    def usun():
+        print("Usuwanie Transakcji Test")
+
+
+
+
+App = App()
+
+
+
+
+
+
+
+
+
+
+
